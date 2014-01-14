2d66c6ab75af86ee559ac3c63b990e264c320c30
Adam Russell <adamlr6+pub@gmail.com>
2014-01-14 00:52:28 -0500

    Remove Taxable type.

--- Bill.hs
+++ Bill.hs
@@ -2,13 +2,10 @@ module Bill where
 
 import Tax
 
-newtype Taxable = Taxable { tax' :: Double -> Double }
-
-bill :: Taxable -> [Double] -> Double
-bill taxable prices =
+bill :: (Double -> Double) -> [Double] -> Double
+bill tax' prices =
   let
-    tax'' = tax' taxable
     pricesSum = sum prices
-    taxOfPrices = tax'' pricesSum
+    taxOfPrices = tax' pricesSum
   in
     pricesSum + taxOfPrices
