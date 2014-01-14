078a2d1366e107cfd5c0f97c98136dd35950f4c0
Adam Russell <adamlr6+pub@gmail.com>
2014-01-14 00:57:02 -0500

    Use `do` notation in `bill` function.

--- Bill.hs
+++ Bill.hs
@@ -7,10 +7,10 @@ import Tax
 newtype Taxable = Taxable { tax' :: Double -> Double }
 
 bill :: [Double] -> Reader Taxable Double
-bill prices = ReaderT $ \taxable ->
+bill prices =
   let
     totalPrice = sum prices
-    tax'' = tax' taxable
-    taxOfPrices = tax'' totalPrice
-  in
+  in do
+    tax'' <- asks tax'
+    let taxOfPrices = tax'' totalPrice
     return $ totalPrice + taxOfPrices
