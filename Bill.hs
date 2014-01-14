f9a3ca0237b536e2d1207a70d37f810cfe8efbc1
Adam Russell <adamlr6+pub@gmail.com>
2014-01-14 00:32:52 -0500

    Put Taxable into Reader monad.

--- Bill.hs
+++ Bill.hs
@@ -1,14 +1,16 @@
 module Bill where
 
+import Control.Monad.Reader
+
 import Tax
 
 newtype Taxable = Taxable { tax' :: Double -> Double }
 
-bill :: Taxable -> [Double] -> Double
-bill taxable prices =
+bill :: [Double] -> Reader Taxable Double
+bill prices = ReaderT $ \taxable ->
   let
     tax'' = tax' taxable
     pricesSum = sum prices
     taxOfPrices = tax'' pricesSum
   in
-    pricesSum + taxOfPrices
+    return $ pricesSum + taxOfPrices
